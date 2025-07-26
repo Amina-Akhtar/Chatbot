2f6cc542479fe262365808e46ac04d4dd0295faa
Amina-Akhtar <330amina@gmail.com>
2025-07-26 22:37:01 +0500

    project structure and complete code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot',
+    version= '0.0.0',
+    author= 'Amina Akhtar',
+    author_email= '330amina@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ src/prompt.py
@@ -0,0 +1,5 @@
+prompt="You are an expert assistant to answer the questions based on the pdf content. " \
+"Use only the following extracted information  to answer the questions. " \
+"If you do not know the answer say: I could not find any relevant information in the document. " \
+"Keep the answers concise, use 3 sentences maximum." \
+"{context}"
